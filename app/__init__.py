4ac17b2df640b4c1daf9f4842bdcdf7d94c0870a
waimea-nawaite <nawaite@waimea.school.nz>
2026-05-13 15:09:20 +1200

    signup login working

--- app/__init__.py
+++ app/__init__.py
@@ -19,13 +19,89 @@ app = Flask(__name__)
 #===========================================================
 # App Routes Handlers
 #===========================================================
-
-#-----------------------------------------------------------
-# Welcome page
-#-----------------------------------------------------------
 @app.get("/")
 def show_welcome():
     return render_template("pages/welcome.jinja")
+#-----------------------------------------------------------
+# Signup Page
+#-----------------------------------------------------------
+@app.get("/user/new")
+def show_signup_form():
+    return render_template("pages/user_form.jinja")
+
+#-----------------------------------------------------------
+# Login Page
+#-----------------------------------------------------------
+@app.get("/user/login")
+def show_login_form():
+    return render_template("pages/login_page.jinja")
+
+#-----------------------------------------------------------
+# Handle user signup
+#-----------------------------------------------------------
+@app.post("/user")
+def add_user():
+    forename = request.form.get('forename', '').strip()
+    surname  = request.form.get('surname',  '').strip()
+    username = request.form.get('username', '').strip().lower()
+    password = request.form.get('password', '').strip()
+
+    with connect_db() as db:
+        sql = "SELECT id FROM users WHERE username=?"
+        params = (username,)
+        user = db.execute(sql, params).fetchone()
+
+        if user:
+            flash(f"Username '{username}' already exists", "error")
+            return redirect("/user/new")
+
+        password_hash = generate_password_hash(password)
+
+        sql = """
+            INSERT INTO users (forename, surname, username, password_hash)
+            VALUES (?, ?, ?, ?)
+        """
+        params = (forename, surname, username, password_hash)
+        db.execute(sql, params)
+
+        flash("Account created. Please login", "success")
+        return redirect("/login")
+
+#-----------------------------------------------------------
+# Handle user login
+#-----------------------------------------------------------
+    
+@app.post("/login")
+def login_user():
+    username = request.form.get('username', '').strip().lower()
+    password = request.form.get('password', '').strip()
+
+    with connect_db() as db:
+        sql = """
+            SELECT id, forename, surname, password_hash
+            FROM users
+            WHERE username=?
+        """
+        params = (username,)
+        user = db.execute(sql, params).fetchone()
+
+        if not user:
+            flash(f"Unknown user", "error")
+            return redirect("/login")
+
+        if not check_password_hash(user["password_hash"], password):
+            flash(f"Incorrect password", "error")
+            return redirect("/login")
+
+        session["logged_in"] = True
+        session["users"] = {
+            "username": username,
+            "forename": user["forename"],
+            "surname":  user["surname"],
+        }
+
+        flash("Login successful", "success")
+        return redirect("/")
 
 
 #-----------------------------------------------------------
